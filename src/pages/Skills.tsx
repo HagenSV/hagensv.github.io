e43aaddd405c091a8b0cf09f300d2115bd6989ec
HAGENSV22 <hagensv22@gcc.edu>
2025-03-26 18:07:29 -0400

    Dynamic page generation

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -1,80 +1,33 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const programmingLanguages = ["Java","JavaScript","HTML/CSS","Scala","Swift","Python","C"];
+const toolsPlatforms = ["Linux/Bash","Git/Github","Junit","Mathematica","Java Swing","Discord.js","Flask","Socketio","Bootstrap","React"];
 
 const SkillsPage: React.FC = () => {
     return (
         <main>
             <h1 className="type-cursor">Skills</h1>
-
             <p className="bold fade-in">Programming Languages:</p>
-            <div className="container text-center">
-                <div className="row justify-content-center" id="nav">
-                    <div className="col-6 col-md-3 fade-in">
-                        <p>Java</p>
-                    </div>
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "0.1s" }}>
-                        <p>JavaScript</p>
-                    </div>
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "0.2s" }}>
-                        <p>HTML/CSS</p>
-                    </div>
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "0.3s" }}>
-                        <p>Scala</p>
-                    </div>
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "0.4s" }}>
-                        <p>Swift</p>
-                    </div>
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "0.5s" }}>
-                        <p>Python</p>
-                    </div>
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "0.6s" }}>
-                        <p>C</p>
-                    </div>
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "0.6s" }}>
-                        <p></p>
-                    </div>
-                </div>
-            </div>
+            <Container className="text-center">
+                <Row>
+                    {programmingLanguages.map((language, index) => (
+                        <div className="col-6 col-md-3 fade-in" style={{ animationDelay: `${index * 0.1}s` }} key={language}>
+                            <p>{language}</p>
+                        </div>
+                    ))}
+                </Row>
+            </Container>
             <p className="bold fade-in" style={{ animationDelay: "1.1s" }}>Tools/Platforms:</p>
-            <div className="container text-center">
-                <div className="row">
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "1.2s" }}>
-                        <p>Linux/Bash</p>
-                    </div>
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "1.3s" }}>
-                        <p>Git/Github</p>
-                    </div>
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "1.4s" }}>
-                        <p>Junit</p>
-                    </div>
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "1.5s" }}>
-                        <p>Mathematica</p>
-                    </div>
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "1.6s" }}>
-                        <p>Java Swing</p>
-                    </div>
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "1.7s" }}>
-                        <p>Discord.js</p>
-                    </div>
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "1.8s" }}>
-                        <p>Flask</p>
-                    </div>
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "1.9s" }}>
-                        <p>Socketio</p>
-                    </div>
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "2.0s" }}>
-                        <p>Bootstrap</p>
-                    </div>
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "0.1s" }}>
-                        <p></p>
-                    </div>
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "0.1s" }}>
-                        <p></p>
-                    </div>
-                    <div className="col-6 col-md-3 fade-in" style={{ animationDelay: "0.1s" }}>
-                        <p></p>
-                    </div>
-                </div>
-            </div>
+            <Container className="text-center">
+                <Row>
+                {toolsPlatforms.map((tool, index) => (
+                        <div className="col-6 col-md-3 fade-in" style={{ animationDelay: `${1.2 + index * 0.1}s` }} key={tool}>
+                            <p>{tool}</p>
+                        </div>
+                    ))}
+                </Row>
+            </Container>
         </main>
     );
 };
