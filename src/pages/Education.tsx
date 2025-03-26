9e6b4372589ea7f1731bb95cf615f4dae4fcda3a
HAGENSV22 <hagensv22@gcc.edu>
2025-03-26 18:31:01 -0400

    Programmatically add courses

--- src/pages/Education.tsx
+++ src/pages/Education.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+const courses = ["Java 1 and 2","Data Structures","Parallel Computing","Data Communication","Theory of Algorithms","Web Programming","iOS Development","Software Engineering"];
+
 const Education: React.FC = () => {
     return (
         <main>
@@ -11,13 +13,9 @@ const Education: React.FC = () => {
             <p className="fade-in" style={{ animationDelay: "2s" }}><span className="bold">GPA:</span> 3.57 in major, 3.3 overall</p>
             <p className="fade-in" style={{ animationDelay: "2.5s" }}><span className="bold">Courses:</span></p>
             <ul>
-                <li className="fade-in" style={{ animationDelay: "3.1s" }}>Java 1 and 2</li>
-                <li className="fade-in" style={{ animationDelay: "3.2s" }}>Data Structures</li>
-                <li className="fade-in" style={{ animationDelay: "3.3s" }}>Parallel Computing</li>
-                <li className="fade-in" style={{ animationDelay: "3.4s" }}>Data Communication</li>
-                <li className="fade-in" style={{ animationDelay: "3.5s" }}>Theory of Algorithms</li>
-                <li className="fade-in" style={{ animationDelay: "3.6s" }}>Web Programming</li>
-                <li className="fade-in" style={{ animationDelay: "3.7s" }}>iOS Development</li>
+                {courses.map((course, index) => (
+                    <li className="fade-in" style={{ animationDelay: `${3 + index * 0.1}s` }} key={course}>{course}</li>
+                ))}
             </ul>
         </main>
     );
