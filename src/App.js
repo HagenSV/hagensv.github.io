2437649442808679f5276208e0fb7d2c554793a2
HAGENSV22 <hagensv22@gcc.edu>
2025-03-26 23:43:30 -0400

    Add 404 to route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Education from './pages/Education';
 import SkillsPage from './pages/Skills';
 import Projects from './pages/Projects';
+import NotFound from './pages/NotFound';
 import Footer from './components/Footer';
 
 function App() {
@@ -14,8 +15,7 @@ function App() {
             <Route index element={<Education />} />
             <Route path="skills" element={<SkillsPage />} />
             <Route path="projects" element={<Projects />} />
-            {/* <Route path="contact" element={<Contact />} />
-            <Route path="*" element={<NoPage />} /> */}
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
