baba061e69266efad3c23df3ff878f04ee7ccf89
HAGENSV22 <hagensv22@gcc.edu>
2025-03-26 23:28:51 -0400

    Add projects route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Education from './pages/Education';
 import SkillsPage from './pages/Skills';
+import Projects from './pages/Projects';
 import Footer from './components/Footer';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/">
             <Route index element={<Education />} />
             <Route path="skills" element={<SkillsPage />} />
+            <Route path="projects" element={<Projects />} />
             {/* <Route path="contact" element={<Contact />} />
             <Route path="*" element={<NoPage />} /> */}
           </Route>
