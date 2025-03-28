728f0379fcdb5d0fd52da05e9840d98816883c72
HAGENSV22 <hagensv22@gcc.edu>
2025-03-28 14:50:58 -0400

    Add routes

--- src/app/App.js
+++ src/app/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Education from './pages/Education';
 import SkillsPage from './pages/Skills';
 import Projects from './pages/Projects';
+import { ProjectDetails } from './pages/ProjectDetails'; // Import the ProjectDetails component
 import NotFound from './pages/NotFound';
 import Footer from './components/Footer';
+import projects from '../projects/index';
 
 function App() {
   return (
@@ -14,7 +16,12 @@ function App() {
           <Route path="/">
             <Route index element={<Education />} />
             <Route path="skills" element={<SkillsPage />} />
-            <Route path="projects" element={<Projects />} />
+            <Route path="projects">
+              <Route index element={<Projects />} />
+              {Object.keys(projects).map((key) => (
+                <Route path={key} element={<ProjectDetails project={projects[key]} />} />
+              ))}
+            </Route>
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
