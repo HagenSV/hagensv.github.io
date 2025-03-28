b6175a7db0099621b3ecae3c1d8fd1e752576169
HAGENSV22 <hagensv22@gcc.edu>
2025-03-28 14:50:40 -0400

    Project data

---
+++ src/projects/index.ts
@@ -0,0 +1,8 @@
+import spaceMiner from "./space_miner";
+
+const projects = {
+    "space-miner": spaceMiner,
+}
+
+
+export default projects
---
+++ src/projects/space_miner.ts
@@ -0,0 +1,14 @@
+import { ProjectData } from "../app/pages/ProjectDetails"
+
+const spaceMiner: ProjectData = {
+    title: "Space Miner",
+    year: 2022,
+    type: "Class, Partner",
+    tools: ["Java", "Java Swing", "Git"],
+    accomplishments: ["Team lead, responsible for assignment submissions","Created github repository and configured branch rules","Used React and Typescript to create a responsive web app","Used Java and Spring Boot for the backend","Used Github for version control and collaboration"],
+    about: "",
+    images: ["/public/images/space_miner/space_miner_1.png", "/public/images/space_miner/space_miner_2.png", "/public/images/space_miner/space_miner_3.png"], // Array of image URLs for the image gallery
+    youtubeLink: null
+}
+
+export default spaceMiner;
