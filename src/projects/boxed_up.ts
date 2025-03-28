9b4bce687479b6570a732166ba5b1500257c52a2
HAGENSV22 <hagensv22@gcc.edu>
2025-03-28 15:07:07 -0400

    Add page data

---
+++ src/projects/boxed_up.ts
@@ -0,0 +1,14 @@
+import { ProjectData } from "../app/pages/ProjectDetails"
+
+const boxedUp: ProjectData = {
+    title: "Boxed Up",
+    year: 2024,
+    type: "Personal, Solo",
+    tools: ["Javascript", "HTML/CSS", "Electron", "Socketio", "Node.js", "Express"],
+    accomplishments: ["Created locally hosted multiplayer game using socket.io and express", "Taught myself electron to create a desktop app", "Used HTML/CSS for the frontend", "Published game to gamejolt.com", "Used Github for version control"],
+    about: "",
+    images: null,
+    youtubeLink: null
+}
+
+export default boxedUp;
--- src/projects/index.ts
+++ src/projects/index.ts
@@ -1,8 +1,11 @@
+import boxedUp from "./boxed_up";
+import ourGcc from "./ourgcc";
 import spaceMiner from "./space_miner";
 
 const projects = {
+    "boxed-up": boxedUp,
+    "ourgcc": ourGcc,
     "space-miner": spaceMiner,
 }
-
 
 export default projects
