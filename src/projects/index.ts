bc2915124b6ca8920df66e4dd8d6dd3f5fd092e0
HAGENSV22 <hagensv22@gcc.edu>
2025-03-28 15:17:55 -0400

    Programattically assign routes

--- src/projects/index.ts
+++ src/projects/index.ts
@@ -1,11 +1,15 @@
+import { ProjectData } from "../types/ProjectData";
 import boxedUp from "./boxed_up";
 import ourGcc from "./ourgcc";
 import spaceMiner from "./space_miner";
 
-const projects = {
-    "boxed-up": boxedUp,
-    "ourgcc": ourGcc,
-    "space-miner": spaceMiner,
+const toRoute = (project: ProjectData): string => {
+    return project.title.toLowerCase().replace(/\s+/g, "-");
 }
+
+const projects: Record<string,ProjectData> = {}
+projects[toRoute(boxedUp)] = boxedUp;
+projects[toRoute(ourGcc)] = ourGcc;
+projects[toRoute(spaceMiner)] = spaceMiner
 
 export default projects
