afb7e15b915e49b7adb036ea60e15886a002c770
HAGENSV22 <hagensv22@gcc.edu>
2025-03-29 09:59:39 -0400

    Add website to projects

--- src/projects/index.ts
+++ src/projects/index.ts
@@ -1,8 +1,9 @@
 import boxedUp from "./boxed_up";
 import ourGcc from "./ourgcc";
+import portfolioWebsite from "./portfolio_website";
 import studentScheduler from "./student_scheduler";
 import spaceMiner from "./space_miner";
 
-const projects = [studentScheduler, boxedUp, ourGcc, spaceMiner]
+const projects = [studentScheduler, portfolioWebsite, ourGcc, boxedUp, spaceMiner]
 
 export default projects
